fd053cba74740f1e3a5ff632467c649b7f7cb6fa
Martand Mahajan <aditryaozalwarcnp@gmail.com>
2024-06-11 23:39:26 +0530

    worked on responsiveness

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,27 +4,36 @@ import logoImage from '../assests/image 3.png';
 
 class Navbar extends Component {
   state = { clicked: false };
-  handleSignup =() =>{
-    window.location.href='/signup';
+
+  handleSignup = () => {
+    window.location.href = '/signup';
   };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
   render(){
     return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={logoImage} alt="ChefConnectIndia" /> 
-      </div>
-      <div className="navbar-links">
-        <a href="/" className="navbar-link">Home</a>
-        <a href="/services" className="navbar-link">Our Services</a>
-        <a href="/contact" className="navbar-link">Contact us</a>
-        <a href="/gallery" className="navbar-link">Gallery</a>
-      </div>
-      <div className='hireNowParent'>
-        <div className='hireNow' onClick={this.handleSignup}>Log In</div>
-      </div>
-    </nav>
-  );
-}
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img src={logoImage} alt="ChefConnectIndia" /> 
+        </div>
+        <div className={`navbar-links ${this.state.clicked ? 'active' : ''}`}>
+          <a href="/" className="navbar-link">Home</a>
+          <a href="/services" className="navbar-link">Our Services</a>
+          <a href="/contact" className="navbar-link">Contact us</a>
+          <a href="/gallery" className="navbar-link">Gallery</a>
+        </div>
+        <div className='hireNowParent' onClick={this.handleSignup}>
+          <div className='hireNow'>Log In</div>
+        </div>
+        <div className="navbar-toggle" onClick={this.handleClick}>
+          ☰
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
